d61dc4586ef69f7a936c866b51ca547245048d5a
Someone <someone@somenet.org>
2021-05-05 01:14:06 +0200

    maintenance/db.py

--- maintenance/db.py
+++ maintenance/db.py
@@ -14,7 +14,7 @@ import psycopg2
 import psycopg2.extras
 
 import config
-print("Mattermost DB cleanup script. Tested on 5.33\n")
+print("Mattermost DB cleanup script. Tested on 5.34\n")
 
 dbconn = psycopg2.connect(config.dbconnstring)
 dbconn.set_session(autocommit=False)
